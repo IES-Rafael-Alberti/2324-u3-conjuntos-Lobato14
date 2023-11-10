f5517d70e300e07caab95aa5b640c608ec2a1557
rubencelislobato@gmail.com <rubencelislobato@gmail.com>
2023-11-10 12:13:33 +0100

    Cambios realizados tes_ej5.py

--- test/test_ej5.py
+++ test/test_ej5.py
@@ -1,4 +1,4 @@
-from src.Ejercicio_5 import conjunto_pares, multiplos_tres, intersecion_pares_multiplos
+from src.Ejercicio_5 import conjunto_pares, multiplos_tres, interseccion_pares_multiplos
 
 def test_conjunto_pares():
     assert conjunto_pares({1, 2, 3, 4, 5, 6, 7, 8, 9, 10}) == {2, 4, 6, 8, 10}
@@ -7,4 +7,4 @@ def test_multiplos_tres():
     assert multiplos_tres({1, 2, 3, 4, 5, 6, 7, 8, 9, 10}) == {3, 6, 9}
 
 def test_intersecion_pares_multiplos():
-    assert intersecion_pares_multiplos({1, 2, 3, 4, 5, 6, 7, 8, 9, 10}) == {6}
+    assert interseccion_pares_multiplos({1, 2, 3, 4, 5, 6, 7, 8, 9, 10}) == {6}
