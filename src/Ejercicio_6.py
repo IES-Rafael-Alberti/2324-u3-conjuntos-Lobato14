4c60e3cdb6420e92a9e8eaa41e3fc656f1f4ecdc
rubencelislobato@gmail.com <rubencelislobato@gmail.com>
2023-11-13 09:25:38 +0100

    Cambios realizados Ejercicio_6.py y en el test_ej6.py

--- src/Ejercicio_6.py
+++ src/Ejercicio_6.py
@@ -10,7 +10,8 @@ def encontrar_consonantes() -> set:
     """
     Encuentra las consonantes en el alfabeto.
 
-    Returns:
+    Returns
+    -------
     - set:
         Conjunto de consonantes en el alfabeto.
     """
@@ -19,27 +20,50 @@ def encontrar_consonantes() -> set:
     consonantes = alfabeto - vocales
     return consonantes
 
-def encontrar_letras_comunes(consonantes: set, vocales: set) -> set:
+def obtener_palabra_valida() -> str:
     """
-    Encuentra las letras comunes entre consonantes y vocales.
+    Solicita al usuario que introduzca una palabra válida (solo letras).
 
-    Parameters:
+    Returns
+    -------
+    - str:
+        Palabra válida introducida por el usuario.
+    """
+    palabra = input("Introduce una palabra: ").lower()
+    while not palabra.isalpha():
+        palabra = input("Por favor, introduce solo letras. Vuelve a intentarlo: ").lower()
+    return palabra
+
+def encontrar_letras_comunes(palabra: str, consonantes: set, vocales: set) -> set:
+    """
+    Encuentra las letras comunes entre consonantes y vocales en una palabra.
+
+    Parameters
+    ----------
+    - palabra : str
+        Palabra de entrada.
     - consonantes : set
         Conjunto de consonantes.
     - vocales : set
         Conjunto de vocales.
 
-    Returns:
+    Returns
+    --------
     - set:
-        Conjunto de letras comunes entre consonantes y vocales.
+        Conjunto de letras comunes entre consonantes y vocales en la palabra.
     """
-    letras_comunes = vocales.intersection(consonantes)
-    return letras_comunes
+    letras_palabra = set(letra for letra in palabra if letra.isalpha())
+    letras_consonantes = letras_palabra.intersection(consonantes)
+    return letras_palabra, letras_consonantes
 
 if __name__ == "__main__":
+    # Entrada
     vocales = {'a', 'e', 'i', 'o', 'u'}
+    # Procesamiento
     consonantes = encontrar_consonantes()
-    letras_comunes = encontrar_letras_comunes(consonantes, vocales)
-
+    palabra = obtener_palabra_valida()
+    letras_palabra, letras_consonantes = encontrar_letras_comunes(palabra, consonantes, vocales)
+    # Salida
     print("Conjunto de consonantes:", consonantes)
-    print("Conjunto de letras comunes:", letras_comunes)
+    print("Conjunto de letras en la palabra:", letras_palabra)
+    print("Conjunto de consonantes en la palabra:", letras_consonantes)
